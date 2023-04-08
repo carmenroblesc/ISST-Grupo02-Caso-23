761d8c439ae40eda38e98ec0fd18152328d516f3
jserranoz98 <juanserrano98@gmail.com>
2023-04-08 14:27:44 +0200

    CitaController.java - Crear cita

--- MedConApp/medconweb/src/main/java/es/upm/dit/isst/medconweb/controller/CitaController.java
+++ MedConApp/medconweb/src/main/java/es/upm/dit/isst/medconweb/controller/CitaController.java
@@ -1,5 +1,44 @@
 package es.upm.dit.isst.medconweb.controller;
 
+import java.util.Map;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import es.upm.dit.isst.medconweb.model.*;
+
 public class CitaController {
-    
+    public final String CITAMANAGER_STRING = "http://localhost:8083/citas/";
+    //"main menu" del ordenador del medico
+    public static final String VISTA_LISTA = "lista";
+    //formulario para concertar cita
+    public static final String VISTA_FORMULARIO = "GestionCitas";
+    private RestTemplate restTemplate = new RestTemplate();
+
+    //1. CREAR CITA
+    //1.1 Devolver Formulario
+    @GetMapping("/crear")
+    public String crear(Map<String, Object> model) {
+        Cita Cita = new Cita();
+        model.put("Cita", Cita);
+        model.put("accion", "guardar");
+        return VISTA_FORMULARIO;
+    }
+
+    //1.2 Validar Formulario y guardar cita
+    @PostMapping("/guardar")
+    public String guardar(@Validated Cita Cita, BindingResult result) {
+        if (result.hasErrors()) {
+            return VISTA_FORMULARIO;
+        }
+        try {
+            restTemplate.postForObject(CITAMANAGER_STRING, Cita, Cita.class);
+        } catch (Exception e) {
+        }
+        return "redirect:" + VISTA_LISTA;
+    }
+
 }
