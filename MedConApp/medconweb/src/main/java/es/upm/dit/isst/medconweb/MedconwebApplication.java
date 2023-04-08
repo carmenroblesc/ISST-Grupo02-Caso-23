797e1b4500aef6d8181d338b321441b87c4792ec
CovadongaDiez <covadiezc@gmail.com>
2023-04-08 11:51:20 +0200

    capa web

---
+++ MedConApp/medconweb/src/main/java/es/upm/dit/isst/medconweb/MedconwebApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.medconweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedconwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedconwebApplication.class, args);
+	}
+
+}
---
+++ MedConApp/medconweb/src/test/java/es/upm/dit/isst/medconweb/MedconwebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.medconweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedconwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
